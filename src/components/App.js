4da95f79cf03a8c0578ff8166dc01cdb6b95041b
Elvis <elvis.omondi@student.moringaschool.com>
2024-09-14 16:01:00 +0300

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,37 @@
-import React from "react";
+import React from 'react';
+import Home from './Home';
+import About from './About';
+
+const App = () => {
+  // Example user object
+  const user = {
+    name: 'John Doe',
+    age: 30,
+    location: 'New York',
+    github: 'https://github.com/johndoe',
+    linkedin: 'https://linkedin.com/in/johndoe',
+    bio: 'Software developer with a passion for open source.'
+  };
+
+  return (
+    <div>
+      <Home
+        name={user.name}
+        age={user.age}
+        location={user.location}
+      />
+      <About
+        bio={user.bio}
+        github={user.github}
+        linkedin={user.linkedin}
+      />
+    </div>
+  );
+};
+
+export default App;
+
+/*import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
@@ -17,4 +50,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
